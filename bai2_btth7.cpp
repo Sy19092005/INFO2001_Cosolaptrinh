c4d4ed22dcea382a004ecf5be9f55a2a83d58cd1
Sy19092005 <148088683+Sy19092005@users.noreply.github.com>
2023-12-11 23:25:38 +0700

    Add files via upload

---
+++ bai2_btth7.cpp
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// Ðinh nghia cau trúc du li?u
+struct Data {
+    int id;
+    float value;
+};
+
+int main() {
+    // Ghi du lieu vào tep tin nhi phân
+    FILE *file = fopen("data.txt", "wb");
+
+    if (file == NULL) {
+        printf("Khong the mo hoac tao file.\n");
+        return 1;
+    }
+
+    struct Data data1 = {1, 3.15};
+    struct Data data2 = {2, 3.06};
+
+    // Ghi du lieu vào tep tin
+    fwrite(&data1, sizeof(struct Data), 1, file);
+    fwrite(&data2, sizeof(struct Data), 1, file);
+
+    // Ðóng tep tin
+    fclose(file);
+
+    // Ðoc du lieu tu tep tin nhi phân và hien thi ra màn hình
+    file = fopen("data.txt", "rb");
+
+    if (file == NULL) {
+        printf("Khong the mo file.\n");
+        return 1;
+    }
+
+    struct Data readData;
+
+    // Ðoc du lieu tu tep tin
+    while (fread(&readData, sizeof(struct Data), 1, file) == 1) {
+        // Hien thi du lieu ra màn hình
+        printf("ID: %d, Value: %.2f\n", readData.id, readData.value);
+    }
+
+    // Ðóng tep tin
+    fclose(file);
+
+    return 0;
+}
+
